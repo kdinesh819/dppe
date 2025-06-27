acdea8f24e9cabd02f87ded3ac4698a629522df4
Dinesh Kumar <154876231+kdinesh819@users.noreply.github.com>
2025-06-27 10:59:36 +0530

    Update main1.bash

--- main1.bash
+++ main1.bash
@@ -1,7 +1,7 @@
-#!/bin/bash
+e#!/bin/bash
 echo "" > ${BASE_DIR}nohup.out
 ### Main file
-BASE_DIR=/root/kirapanel1/
+BASE_DIR=/root/dedupe/
 PY_BIN=/usr/bin/python3
 nohup ${PY_BIN} ${BASE_DIR}main.py &
 
